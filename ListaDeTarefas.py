e66dd675418423c7a087ebfbe1b53346099701b4
Vinicius Aguiar <vinigadelha123@gmail.com>
2025-05-30 12:08:46 -0300

    refatoramento em processo

--- ListaDeTarefas.py
+++ ListaDeTarefas.py
@@ -4,17 +4,17 @@
 # Lista base
 lista_de_tarefas = []
 
-while True:
+# Evitar usar while para nao causar processamento infinito.
 
-    # Visualização da lista
-    estrela = '-'*30
-    print('    LISTA-DE-TAREFAS   ')
-    if lista_de_tarefas == ['']:
+ # Visualização da lista
+ estrela = '-'*30
+  print('    LISTA-DE-TAREFAS   ')
+   if lista_de_tarefas == ['']:
         print('     Lista vazia')
     else:
         for tarefa in lista_de_tarefas:
             print(lista_de_tarefas.index(tarefa)+1, tarefa, sep='-')
-
+# usar operador logico para tratar
 
 # Estabelecer menu onde há as opções:
 # (lista exibida)
@@ -29,56 +29,63 @@ while True:
             'Remover uma tarefa', 'Limpar a lista', 'Fechar']
     for i, item in enumerate(menu, start=1):
         print(f"{i}. {item}")
-    opcao = input('O que deseja fazer?: ')
+    opcao = int(input('O que deseja fazer?: '))
 
-# Preencher a lista
-    if opcao == '1':
-        while True:
-            tarefa_add = input('escreva a tarefa a ser adicionada: ')
-            if tarefa_add == '':
-                print('Sua tarefa está vazia')
-            else:
-                lista_de_tarefas.append(tarefa_add)
-                print('tarefa adicionada')
-                Tarefa_adicional = input(
-                    'deseja adicionar mais tarefas, sim ou não: ')
-                if Tarefa_adicional == 'sim':
-                    print('adicione mais tarefas: ')
-                elif Tarefa_adicional == 'nao':
-                    break
+# Passar  parametro do input como uma variavel transformar para inteiro
+# fazer um switch case
+# criar função para chamar função quando chamar fnução
+# usar case para evitar repetir tratamento de erro
+# dividir os ifs em funções
 
-    # Alteração da lista
-    elif opcao == '2':
-        while True:
-            print('    LISTA-DE-TAREFAS   ')
-            for tarefa in lista_de_tarefas:
-                print(lista_de_tarefas.index(tarefa)+1, tarefa, sep='-')
-            opcao_delete = input(
-                'Informe o número da tarefa que deseja excluir: ')
-            if opcao_delete == '':
-                print('Opção inválida, tente de novo')
-            else:
-                decisao = input(
-                    'deseja realmente limpar a lista de tarefas? sim ou não: ')
-                if decisao == 'sim':
-                    indice_deletado = int(opcao_delete)-1
-                    lista_de_tarefas.pop(indice_deletado)
-                    print('tarefa deletada')
-                elif decisao == 'nao':
-                    break
-                elif decisao == str:
-                    print('opção invalida')
+    match opcao:
 
-    # Limpar lista
-    elif opcao == '3':
-        decisao = input(
-            'deseja realmente limpar a lista de tarefas? sim ou não: ')
-        if decisao == 'sim':
-            lista_de_tarefas.clear()
-        elif decisao == 'nao':
-            break
-        elif decisao == str:
+        # Preencher a lista
+        case 1:
+            while True:
+                tarefa_add = input('escreva a tarefa a ser adicionada: ')
+                if tarefa_add == '':
+                    print('Sua tarefa está vazia')
+                else:
+                    lista_de_tarefas.append(tarefa_add)
+                    print('tarefa adicionada')
+                    Tarefa_adicional = input(
+                        'deseja adicionar mais tarefas, sim ou não: ')
+                    if Tarefa_adicional == 'sim':
+                        print('adicione mais tarefas: ')
+                    elif Tarefa_adicional == 'nao':
+
+                 # Alteração da lista
+        case 2:
+            while True:
+                print('    LISTA-DE-TAREFAS   ')
+                for tarefa in lista_de_tarefas:
+                    print(lista_de_tarefas.index(tarefa)+1, tarefa, sep='-')
+                opcao_delete = input(
+                    'Informe o número da tarefa que deseja excluir: ')
+                if opcao_delete == '':
+                    print('Opção inválida, tente de novo')
+                else:
+                    decisao = input(
+                        'deseja realmente limpar a lista de tarefas? sim ou não: ')
+                    if decisao == 'sim':
+                        indice_deletado = int(opcao_delete)-1
+                        lista_de_tarefas.pop(indice_deletado)
+                        print('tarefa deletada')
+                    elif decisao == 'nao':
+                        print('opção invalida')
+                    elif decisao == str:
+                        print('opção invalida')
+
+        # Limpar lista
+        case 3:
+            decisao = input(
+                'deseja realmente limpar a lista de tarefas? sim ou não: ')
+            if decisao == 'sim':
+                lista_de_tarefas.clear()
+            elif decisao == 'nao':
+                print('opção invalida')
+            elif decisao == str:
+                print('opção invalida')
+
+        case _:
             print('opção invalida')
-
-    if opcao == '4':
-        break
