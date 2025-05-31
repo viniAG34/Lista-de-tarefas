ce2e227c63951e395fce525014ff8683ea52a6f1
Vinicius Aguiar <vinigadelha123@gmail.com>
2025-05-31 14:03:04 -0300

    novas funcoes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from controllers.tarefa_controller import Funcoes_tarefa
 import sys
 import os
 from pathlib import Path
@@ -6,34 +7,30 @@ from pathlib import Path
 SRC_DIR = Path(__file__).resolve().parent
 sys.path.insert(0, str(SRC_DIR))
 
-from controllers.tarefa_controller import Funcoes_tarefa
 
 def main():
-    
+
     # Exibir tarefas
-    Funcoes_tarefa.exibir_tarefas()
 
-    #scolha do usuario
-    opcao = int(input('escolha uma opção a seguir'))
-    
-    match opcao:
-        #adicionar nova tarefa
-        case 1:
-            entrada_titulo = input('digite o título da nota: ')
-            entrada_descricao = input('digite a descrição da nota: ')
-            
+    # #scolha do usuario
+    # opcao = int(input('escolha uma opção a seguir'))
 
-            Funcoes_tarefa.salvar_tarefa(entrada_titulo, entrada_descricao)
-    
-    
+    # match opcao:
+    #     #adicionar nova tarefa
+    #     case 1:
+    #         entrada_titulo = input('digite o título da nota: ')
+    #         entrada_descricao = input('digite a descrição da nota: ')
+
+    #         Funcoes_tarefa.adicionar_tarefa(entrada_titulo, entrada_descricao)
+
     # Criar tarefas de exemplo
-    tarefa1 = Funcoes_tarefa.salvar_tarefa("Estudar Python", "Padrões MVC")
-    tarefa2 = Funcoes_tarefa.salvar_tarefa("Compras", "Leite, ovos, pão")
-    
+    tarefa1 = Funcoes_tarefa.adicionar_tarefa("Estudar Python", "Padrões MVC")
+    tarefa2 = Funcoes_tarefa.adicionar_tarefa("Compras", "Leite, ovos, pão")
+
+    Funcoes_tarefa.exibir_tarefas()
     # Marcar uma tarefa como concluída
     tarefa1.status = True
-    
-    
+
 
 if __name__ == "__main__":
     main()
